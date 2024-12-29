83fe07736eecd2f248d28c81f54afc47b7c7e242
Dayem Siddiqui <dayem.siddiqui@personio.de>
2024-12-29 06:29:08 +0000

    feat: add consume api and tests

--- app/src/persistence/queue.rs
+++ app/src/persistence/queue.rs
@@ -65,7 +65,7 @@ impl Queue {
                 txn.delete(&message_key).expect("Failed to delete message from queue");  
 
                 // Store this message in the "processing" queue
-                let processing_key = format!("processing:{}", String::from_utf8_lossy(&message_key));
+                let processing_key = format!("processing:{}", message.id);
                 let message_bytes = message.as_bytes().expect("Failed to serialize message");
                 txn.put(processing_key.as_bytes(), &message_bytes).expect("Failed to store message in processing queue");   
 
@@ -76,4 +76,58 @@ impl Queue {
             Err(e) => Err(e),
         }
     }
-}   
+
+    pub fn ack(&self, message_id: &str) -> bool {
+        let db = get_db().lock().expect("Failed to lock database");
+        let txn = db.transaction();
+        let processing_key = format!("processing:{}", message_id);
+        txn.delete(&processing_key.as_bytes()).expect("Failed to delete message from processing queue");
+        txn.commit().expect("Failed to commit transaction");
+        true
+    }
+
+    pub fn is_processing(&self, message_id: &str) -> bool {
+        let db = get_db().lock().expect("Failed to lock database");
+        let processing_key = format!("processing:{}", message_id);
+        let result = db.get(&processing_key.as_bytes());
+        match result {
+            Ok(Some(_)) => true,
+            _ => false,
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_consume_and_ack() {
+        let queue = Queue::get("test_queue".to_string());
+        let message = Message::new("test_message".to_string()).expect("Failed to create message");
+        queue.enqueue(&message).expect("Failed to enqueue message");
+        queue.ack(&message.id);
+        let result = queue.consume();
+        let response = match result {
+            Ok(Some(message)) => message,
+            Ok(None) => panic!("Expected to receive a message"),
+            Err(e) => panic!("Expected to receive a message"),
+        }; 
+        assert!(response.id == message.id);
+
+        // Assert that the message is no longer in the queue
+        let result = queue.consume();
+        assert!(result.is_ok());
+        assert!(result.unwrap().is_none());
+
+        // Assert that the message is still in the processing queue
+        assert!(queue.is_processing(&message.id));
+
+        // Ack the message
+        queue.ack(&message.id);
+
+        // Assert that the message is no longer in the processing queue
+        assert!(!queue.is_processing(&message.id));
+    }
+}
